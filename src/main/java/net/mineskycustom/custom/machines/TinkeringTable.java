fc8ffc56f90ec5ce521e837052acc8f9a6562951
drawned <drawnwylde@gmail.com>
2026-09-08 23:10:36 -0300

    tinkering advancement

--- src/main/java/net/mineskycustom/custom/machines/TinkeringTable.java
+++ src/main/java/net/mineskycustom/custom/machines/TinkeringTable.java
@@ -1,5 +1,6 @@
 package net.mineskycustom.custom.machines;
 
+import net.minesky.mineskygameplay.advancements.AdvancementsAPI;
 import net.mineskycustom.MineSkyCustom;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
@@ -9,6 +10,7 @@ public class TinkeringTable {
     public static void openMineSkyItemsGUI(final Player player, Block origin) {
         try {
             net.mineskyitems.gui.tinkering.TinkeringGUI.openGUI(player, origin);
+            AdvancementsAPI.get().grantAsync(player, "minesky:invencao/aprendiz");
         } catch(Exception ex) {
             MineSkyCustom.l.severe("Método TinkeringGUI retornou erro: "+ex.getMessage());
         }
